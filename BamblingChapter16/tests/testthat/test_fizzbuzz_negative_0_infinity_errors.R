af102cb0c62b3b7e1d4237be3b8a550ce17a43dc
bambling-kyle <151088649+bambling-kyle@users.noreply.github.com>
2023-12-07 14:55:36 -0700

    Delete BamblingChapter16 directory

--- BamblingChapter16/tests/testthat/test_fizzbuzz_negative_0_infinity_errors.R
+++
@@ -1,6 +0,0 @@
-library(testthat)
-testthat::test_that('Throws an error when n has a negative, 0, or infinite value', {
-  expect_error(fizz_buzz(-1:3))
-  expect_error(fizz_buzz(0:3))
-  expect_error(fizz_buzz(Inf))
-})
